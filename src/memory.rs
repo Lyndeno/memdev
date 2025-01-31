b7ed52891f6e7cb370e5a322f71f5d92fd148ec0
Lyndon Sanche <lsanche@lyndeno.ca>
2025-01-31 10:27:23 -0700

    Add enum for mem type

--- src/memory.rs
+++ src/memory.rs
@@ -18,8 +18,42 @@ pub struct MemDevice {
     pub manufacturer: Option<String>,
     pub frequency: Option<u64>,
     pub form_factor: Option<String>,
-    pub mem_type: Option<String>,
+    pub mem_type: MemType,
     pub extra_props: HashMap<String, String>,
+}
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone)]
+pub enum MemType {
+    Ddr5,
+    Ddr4,
+    Ddr3,
+    Unknown,
+    Other(String),
+}
+
+#[allow(clippy::enum_glob_use)]
+impl From<String> for MemType {
+    fn from(value: String) -> Self {
+        use MemType::*;
+        match value.as_str() {
+            "DDR5" => Ddr5,
+            "DDR4" => Ddr4,
+            "DDR3" => Ddr3,
+            "Unknown" => Unknown,
+            _ => Other(value),
+        }
+    }
+}
+
+impl std::fmt::Display for MemType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            MemType::Other(v) => v.to_string(),
+            t => format!("{t:?}"),
+        };
+        write!(f, "{s}")
+    }
 }
 
 impl MemDevice {
@@ -47,7 +81,9 @@ impl From<HashMap<String, String>> for MemDevice {
             .remove("CONFIGURED_SPEED_MTS")
             .and_then(|x| str::parse::<u64>(&x).ok());
         let form_factor = extra_props.remove("FORM_FACTOR");
-        let mem_type = extra_props.remove("TYPE");
+        let mem_type = extra_props
+            .remove("TYPE")
+            .map_or(MemType::Unknown, Into::into);
 
         Self {
             manufacturer,
